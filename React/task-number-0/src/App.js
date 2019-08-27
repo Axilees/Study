d07bf765953c166c6e5ab121c48d27e5d3da8d0f
Konstantin Tuezov <Konstantin_Tuezov@rcomext.com>
2019-08-27 17:37:53 +0300

    Added scss

--- React/task-number-0/src/App.js
+++ React/task-number-0/src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component{
     render(){
         return(
             <div className = "App">
-             <h1> Hello, world! </h1>
+             <h1> Sex,Drugs And Rock And Roll!</h1>
             </div>
         );
     }
