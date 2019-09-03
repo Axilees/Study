40c1b503a75a6581b7dbdfccf0b7cad17f74cf88
Konstantin Tuezov <Konstantin_Tuezov@rcomext.com>
2019-09-03 14:42:33 +0300

    Initial commit

---
+++ React/task-number-2/src/App.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class App extends Component{
+    render(){
+        return(
+            <h1>THIS IS APP!</h1>
+        )
+    }
+}
+
+export default App;
