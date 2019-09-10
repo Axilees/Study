ed0d5f1f0fbbaa97be0ab98f78fd47fd803fb864
Konstantin Tuezov <Konstantin_Tuezov@rcomext.com>
2019-09-10 15:37:33 +0300

    Working on context

--- React/task-number-6/src/OutputComponent.js
+++ React/task-number-6/src/OutputComponent.js
@@ -1,10 +1,19 @@
 import React from "react";
+import {ValueContext} from "./ValueContext";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 function OutputComponent(props) {
     return (
-        <div className={props.inputStyle}>
-            <h1>Output Component!</h1>
-        </div>
+        <ValueContext.Consumer>
+            {({value}) => (
+                <div className={props.inputStyle}>
+                    <Paper style={{padding: 20}}>
+                        <Typography variant="h3"> {value} </Typography>
+                    </Paper>
+                </div>
+            )}
+        </ValueContext.Consumer>
     )
 }
 
---
+++ React/task-number-6/src/ValueContext.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const ValueContext = React.createContext({
+    value: "",
+    changeValue: () => {}
+})
